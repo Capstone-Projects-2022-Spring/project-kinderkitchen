2c904ff54315b930561359db95ed4d8bad034011
Abdullah_Arishi <55096042+Arishi2000@users.noreply.github.com>
2022-03-03 16:17:20 -0500

    Testing this new branch with minor changes

--- kinderkitchen/App.js
+++ kinderkitchen/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Wellcome to Kinder Kitchen app!!!</Text>
+      <Text>Wellcome to Kinderrr Kitchen app!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
