9c67c7d1438855ed7c9767c56ac0b1d3f5148ce3
Abdullah_Arishi <55096042+Arishi2000@users.noreply.github.com>
2022-03-03 20:01:25 -0500

    Adding Username and Password

--- kinderkitchen/App.js
+++ kinderkitchen/App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity
+} from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Wellcome to Kinderrr Kitchen app!!!</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome to Kinder Kitchen</Text>
+          <TextInput
+              style={styles.input}
+              placeholder="Username"
+          />
+          <TextInput
+              style={styles.input}
+              placeholder="Password"
+          />
+          <View>
+              <TouchableOpacity>
+                  <Text>Login</Text>
+              </TouchableOpacity>
+          </View>
     </View>
   );
 }
+          
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1e90ff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    },
+    welcome: {
+        fontSize: 30,
+        textAlign: 'center',
+        margin: 10,
+        color: "#fff",
+        fontFamily: "DancingScript-Bold"
+    },
+    input: {
+        width: "90%",
+        backgroundColor: "#fff",
+        padding: 15,
+        marginBottom: 10
+    }
+
 });
