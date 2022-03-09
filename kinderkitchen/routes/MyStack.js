11e7e78ab38ad5c867efc03f6920a99aac7ed90c
Abdullah_Arishi <55096042+Arishi2000@users.noreply.github.com>
2022-03-09 13:45:51 -0500

    Declaring the Category page and started designing

--- kinderkitchen/routes/MyStack.js
+++ kinderkitchen/routes/MyStack.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import LoginScreen from "../screens/LoginScreen";
-import Test1 from "../screens/Test1";
+import Category from "../screens/Test1";
 import Test2 from "../screens/Test2";
 
 const Stack = createStackNavigator();
@@ -11,7 +11,7 @@ const MyStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="LoginScreen" component={LoginScreen} />
-      <Stack.Screen name="Test1" component={Test1} />
+      <Stack.Screen name="Test1" component={Category} />
       <Stack.Screen name="Test2" component={Test2} />
     </Stack.Navigator>
   );
--- kinderkitchen/screens/Test1.js
+++ kinderkitchen/screens/Test1.js
@@ -3,28 +3,43 @@ import { View, StyleSheet, Text } from "react-native";
 
 import MyNavMenu from "../nav-bar/MyNavMenu";
 
-const Test1 = () => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.body}>
-        <Text>Hello, I'm page 1</Text>
-      </View>
-      <MyNavMenu />
-    </View>
-  );
+const Category = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.title}>Categories</Text>
+            </View>
+            <View style={styles.body}>
+                <Text>Hello, I'm page 1</Text>
+            </View>
+            <MyNavMenu />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  container: {
-    backgroundColor: "skyblue",
-    flex: 1,
-    justifyContent: "center",
-  },
+    body: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    container: {
+
+        backgroundColor: "#E8EAED",
+        flex: 1,
+        justifyContent: "center",
+    },
+    header: {
+        height: 80,
+        paddingTop: 40,
+        paddingHorizontal: 20,
+        backgroundColor: 'coral'
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 22,
+        fontWeight: 'bold'
+    },
 });
 
-export default Test1;
+export default Category;
