5f53d2bd44a423c70b9e074c941056a4a27a45ad
unknown <HunterLaut@gmail.com>
2022-03-11 01:37:26 -0500

    Added UI for Items page, Created Component: Item

--- kinderkitchen/screens/ItemScreen.js
+++ kinderkitchen/screens/ItemScreen.js
@@ -1,19 +1,33 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 
 import MyNavMenu from "../nav-bar/MyNavMenu";
+import Item from "../Components/Item"
+import dummyThiccIngredients from "./DummyData";
+import HeaderComponent from "../Components/HeaderComponent";
 
 const ItemScreen = () => {
+  //Replace CATEGORY_NAME to Category when user clicks on specific field
   return (
     <View style={styles.container}>
-      <View style={styles.body}>
-        <Text>This is where the user will see their items!</Text>
-      </View>
+      <HeaderComponent title = "CATEGORY_NAME" />
+      <ScrollView style={{margin: 5}}>
+        <Item item_name="Milk" />
+        <Item/><Item item_name="Cheese"/>
+        <Item/>
+      </ScrollView>{/*difficulty getting data to transfer to prop*/}
+      
       <MyNavMenu />
     </View>
   );
 };
 
+// //function add all items from category
+// function BootyThicc(){
+//   for(int i=1; i<=3;i++){
+
+//   }
+// }
 const styles = StyleSheet.create({
   body: {
     flex: 1,
