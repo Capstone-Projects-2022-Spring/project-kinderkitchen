9ca8819933507d7f1d40be7de9ebf7053c1b3340
unknown <HunterLaut@gmail.com>
2022-03-11 16:02:36 -0500

    made add item button but it does not update page

--- kinderkitchen/screens/ItemScreen.js
+++ kinderkitchen/screens/ItemScreen.js
@@ -1,26 +1,48 @@
 import React from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, ScrollView, Button, Alert } from "react-native";
 
 import MyNavMenu from "../nav-bar/MyNavMenu";
 import Item from "../Components/Item"
 import dummyThiccIngredients from "./DummyData";
 import HeaderComponent from "../Components/HeaderComponent";
 
-var itemList = [];
+
 
 const ItemScreen = (props) => {
-  //Replace CATEGORY_NAME to Category when user clicks on specific field
+
+  var itemList = [];
+
+  //props.category name
+  //props.category_id
+  //props.obj?
+  //props.item_name
+
+  let NumITEMS = 5; //TEMP until Obj is functional to get item name
+
+  //For Loop to get all Items from (CATEGORY)
+  function showItems() {
+    for (let i = 1; i <= NumITEMS; i++) {
+      itemList.push(
+        <View key={i}>
+          <Item item_name={i} />
+        </View>)
+      console.log("Item: " + "DummyThicc" + i + " added");
+    };
+  }
+  function addItems(){
+    NumITEMS += 1;
+    showItems();
+  }
+
+
+  showItems()
   return (
     <View style={styles.container}>
       {props.category_name ? <HeaderComponent title={props.category_name} /> : <HeaderComponent title="CATEGORY_NAME" />/*If no title provided*/}
       <ScrollView style={{ flex: 1, margin: 5 }}>
         {itemList}
-        <Item item_name="Milk" />
-        {<Item />/*get dummydata to prop*/}
-        <Item item_name="Cheese" />
-        <Item />
       </ScrollView>
-
+      <Button title=" + Add Item" onPress={()=>addItems()}/*needs Styling, Not sure how to Update the page*//>
       <MyNavMenu />
     </View>
   );
