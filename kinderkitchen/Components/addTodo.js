52fc7a777ae27b239e2283f5dbc6c33040cdc7aa
Abdullah_Arishi <55096042+Arishi2000@users.noreply.github.com>
2022-03-13 16:00:09 -0400

    The adding functionality is working well now on the category screen

--- kinderkitchen/Components/addTodo.js
+++ kinderkitchen/Components/addTodo.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button,TouchableOpacity } from "react-native";
 
 
 
-export default function AddTodo() {
+export default function AddTodo({submitHandler}) {
 
     const [text, setText] = useState('');
     
@@ -13,15 +13,26 @@ export default function AddTodo() {
     }
 
     return (
+
+
+
         <View style={styles.footer}>
+
             <TextInput
                 style={styles.TextInput}
-                placeholder='+ Enter Category Name'
+                placeholder='Enter Category Name'
                 onChangeText={changeHandler}
                 placeholderTextColor='#fff'
                 underlineColorAndroid='transparent'>
                 
             </TextInput>
+
+            <Button onPress={() => submitHandler(text) }  title='add'  style={styles.button} />
+                
+            
+
+
+
         </View>
 
       
@@ -39,5 +50,21 @@ const styles = StyleSheet.create({
         backgroundColor: '#252525',
         borderTopWidth: 2,
         borderTopColor: '#ededed'
-    }
+    },
+    button: {
+        position: 'absolute',
+        zIndex: 11,
+        right: 20,
+        bottom: 90,
+        backgroundColor: "coral",
+        width: 90,
+        height: 90,
+        borderRadius: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 8,
+    },
+    addButtonText: {
+        backgroundColor: "#2196F3",
+    },
 });
--- kinderkitchen/screens/CategoryScreen.js
+++ kinderkitchen/screens/CategoryScreen.js
@@ -1,20 +1,21 @@
 
 import React, { useState } from "react";
-import { View, StyleSheet, ScrollView, Text, FlatList, TouchableOpacity } from "react-native";
+import { View, StyleSheet, ScrollView, Text,Button, FlatList, TouchableOpacity } from "react-native";
 
 import MyNavMenu from "../nav-bar/MyNavMenu";
 import { TextInput } from "react-native-gesture-handler";
 import HeaderComponent from "../Components/HeaderComponent";
 import TodoItem from "../Components/TodoItem";
+import AddTodo from "../Components/addTodo";
 
 
 
 const CategoryScreen = (props) => {
 
     const [todos, setTodos] = useState([
-        { text: 'buy coffee', key: '1' },
-        { text: 'Go to the gym', key: '2' },
-        { text: 'Study for Network', key: '3' }
+        { text: 'Pantry', key: '1' },
+        { text: 'Fridge', key: '2' },
+        { text: 'Fruit', key: '3' }
     ]);
 
     {/*   This is a function that does the deleting from the list by long pressing on the item */ }
@@ -22,6 +23,17 @@ const CategoryScreen = (props) => {
         setTodos((prevTodos) => {
             return prevTodos.filter(todo => todo.key != key)
         })
+    }
+
+    const submitHandler = (text) => {
+        setTodos((prevTodos) => {
+            return [
+                {text: text, key: Math.random().toString()},
+                ...prevTodos
+            ]
+
+        })
+
     }
    
     return (
@@ -31,6 +43,8 @@ const CategoryScreen = (props) => {
             
             <View style={styles.content}>
                 {/*   Content   */}
+                <AddTodo submitHandler={submitHandler}/>
+
 
                 <View style={styles.list}>
                     {/*   this list thru array and display   */}
@@ -50,19 +64,7 @@ const CategoryScreen = (props) => {
             </ScrollView>
 
            
-            <View style={styles.footer}>
-                <TextInput
-                    style={styles.TextInput}
-                    placeholder='+ Enter Category Name'
-                    placeholderTextColor='#fff'
-                    underlineColorAndroid='transparent'>
-                </TextInput>
-            </View>
-
-            <TouchableOpacity style={styles.button} >
-                <Text>Add</Text>
-            </TouchableOpacity >
-
+           
 
             <MyNavMenu />
         </View>
@@ -81,30 +83,8 @@ const styles = StyleSheet.create({
         marginTop: 20,
       
     },
-    TextInput: {
-        alignSelf: 'stretch',
-        color: '#fff',
-        padding: 10,
-        backgroundColor: '#252525',
-        borderTopWidth: 2,
-        borderTopColor: '#ededed',
-    },
-    button: {
-        position: 'absolute',
-        zIndex: 11,
-        right: 20,
-        bottom: 90,
-        backgroundColor: "coral",
-        width: 90,
-        height: 90,
-        borderRadius: 50,
-        alignItems: 'center',
-        justifyContent: 'center',
-        elevation:8,
-    },
-    addButtonText: {
-        backgroundColor: "#2196F3",
-    },
+    
+    
 });
 
 export default CategoryScreen;
