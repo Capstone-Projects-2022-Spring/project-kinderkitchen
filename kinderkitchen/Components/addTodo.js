97d3b4d9813c10ee41d49834cada8ea53b0545a0
Ricky Zhou <59936830+ZhouRicky@users.noreply.github.com>
2022-03-13 21:24:37 -0400

    Code Refactor & Cleanup

--- kinderkitchen/Components/addTodo.js
+++ kinderkitchen/Components/addTodo.js
@@ -1,70 +1,62 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button,TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 
+export default function AddTodo({ submitHandler }) {
+  const [text, setText] = useState("");
 
+  const changeHandler = (val) => {
+    setText(val);
+  };
 
-export default function AddTodo({submitHandler}) {
+  return (
+    <View style={styles.footer}>
+      <TextInput
+        style={styles.TextInput}
+        placeholder="Enter Category Name"
+        onChangeText={changeHandler}
+        placeholderTextColor="#fff"
+        underlineColorAndroid="transparent"
+      ></TextInput>
 
-    const [text, setText] = useState('');
-    
-    const changeHandler = (val) => {
-        setText(val)
-
-    }
-
-    return (
-
-
-
-        <View style={styles.footer}>
-
-            <TextInput
-                style={styles.TextInput}
-                placeholder='Enter Category Name'
-                onChangeText={changeHandler}
-                placeholderTextColor='#fff'
-                underlineColorAndroid='transparent'>
-                
-            </TextInput>
-
-            <Button onPress={() => submitHandler(text) }  title='add'  style={styles.button} />
-                
-            
-
-
-
-        </View>
-
-      
-
-
-        )
-
+      <Button
+        onPress={() => submitHandler(text)}
+        title="add"
+        style={styles.button}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    TextInput: {
-        alignSelf: 'stretch',
-        color: '#fff',
-        padding: 10,
-        backgroundColor: '#252525',
-        borderTopWidth: 2,
-        borderTopColor: '#ededed'
-    },
-    button: {
-        position: 'absolute',
-        zIndex: 11,
-        right: 20,
-        bottom: 90,
-        backgroundColor: "coral",
-        width: 90,
-        height: 90,
-        borderRadius: 50,
-        alignItems: 'center',
-        justifyContent: 'center',
-        elevation: 8,
-    },
-    addButtonText: {
-        backgroundColor: "#2196F3",
-    },
+  TextInput: {
+    alignSelf: "stretch",
+    color: "#fff",
+    padding: 10,
+    backgroundColor: "#252525",
+    borderTopWidth: 2,
+    borderTopColor: "#ededed",
+  },
+  button: {
+    position: "absolute",
+    zIndex: 11,
+    right: 20,
+    bottom: 90,
+    backgroundColor: "coral",
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 8,
+  },
+  addButtonText: {
+    backgroundColor: "#2196F3",
+  },
 });
--- kinderkitchen/screens/CategoryScreen.js
+++ kinderkitchen/screens/CategoryScreen.js
@@ -11,28 +11,23 @@ import {
 } from "react-native";
 
 import MyNavMenu from "../nav-bar/MyNavMenu";
-import ItemScreen from "./ItemScreen";
 
 const CategoryScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.title}>Categories</Text>
-      </View>
-
       <View style={styles.body}>
         <ScrollView style={styles.scrollView}>
           {/* Both will lead to the same items screen until we differentiate them /}
           <TouchableHighlight
             style={styles.touchable}
-            onPress={() => navigation.navigate(ItemScreen)}
+            onPress={() => navigation.navigate("Items")}
           >
             <Text>Fridge</Text>
           </TouchableHighlight>
 
           <TouchableHighlight
             style={styles.touchable}
-            onPress={() => navigation.navigate(ItemScreen)}
+            onPress={() => navigation.navigate("Items")}
           >
             <Text>Pantry</Text>
           </TouchableHighlight>
@@ -60,16 +55,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
   },
-  header: {
-    height: 80,
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    backgroundColor: "coral",
-  },
-  title: {
-    textAlign: "center",
-    fontSize: 22,
-    fontWeight: "bold",
+  scrollView: {
+    borderWidth: 1,
+    width: "100%",
+    marginBottom: 20,
   },
   touchable: {
     backgroundColor: "skyblue",
@@ -88,9 +77,16 @@ const styles = StyleSheet.create({
     Author: Dom
 */
 
-
 import React, { useState } from "react";
-import { View, StyleSheet, ScrollView, Text,Button, FlatList, TouchableOpacity } from "react-native";
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  Text,
+  Button,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
 import MyNavMenu from "../nav-bar/MyNavMenu";
 import { TextInput } from "react-native-gesture-handler";
@@ -98,81 +94,72 @@ import HeaderComponent from "../Components/HeaderComponent";
 import TodoItem from "../Components/TodoItem";
 import AddTodo from "../Components/addTodo";
 
-
-
 const CategoryScreen = (props) => {
+  const [todos, setTodos] = useState([
+    { text: "Pantry", key: "1" },
+    { text: "Fridge", key: "2" },
+    { text: "Fruit", key: "3" },
+  ]);
 
-    const [todos, setTodos] = useState([
-        { text: 'Pantry', key: '1' },
-        { text: 'Fridge', key: '2' },
-        { text: 'Fruit', key: '3' }
-    ]);
+  {
+    /*   This is a function that does the deleting from the list by long pressing on the item */
+  }
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
 
-    {/*   This is a function that does the deleting from the list by long pressing on the item */ }
-    const pressHandler = (key) => {
-        setTodos((prevTodos) => {
-            return prevTodos.filter(todo => todo.key != key)
-        })
-    }
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+    });
+  };
 
-    const submitHandler = (text) => {
-        setTodos((prevTodos) => {
-            return [
-                {text: text, key: Math.random().toString()},
-                ...prevTodos
-            ]
+  return (
+    <View style={styles.container}>
+      {/*   This is the Header */}
+      {
+        props.category_name ? (
+          <HeaderComponent title={props.category_name} />
+        ) : (
+          <HeaderComponent title="Categories" />
+        ) /*If no title provided*/
+      }
 
-        })
+      <View style={styles.content}>
+        {/*   Content   */}
+        <AddTodo submitHandler={submitHandler} />
 
-    }
-   
-    return (
-        <View style={styles.container}>
-            {/*   This is the Header */}
-            {props.category_name ? <HeaderComponent title={props.category_name} /> : <HeaderComponent title="Categories" />/*If no title provided*/}
-            
-            <View style={styles.content}>
-                {/*   Content   */}
-                <AddTodo submitHandler={submitHandler}/>
+        <View style={styles.list}>
+          {/*   this list thru array and display   */}
 
-
-                <View style={styles.list}>
-                    {/*   this list thru array and display   */}
-
-                    <FlatList
-                        data={todos}
-                        renderItem={({ item }) => (
-                            <TodoItem item={item} pressHandler={pressHandler} />
-                        )}
-                    />
-                </View>
-            </View>
-
-
-            <ScrollView style={styles.scrollContainer}>
-
-            </ScrollView>
-
-           
-           
-
-            <MyNavMenu />
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
         </View>
-    );
+      </View>
+
+      <ScrollView style={styles.scrollContainer}></ScrollView>
+
+      <MyNavMenu />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    content: {
-        padding:40,
-        
-    },
-    list: {
-        marginTop: 20,
-      
-    },
+  container: {
+    flex: 1,
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
 });
 
 export default CategoryScreen;
