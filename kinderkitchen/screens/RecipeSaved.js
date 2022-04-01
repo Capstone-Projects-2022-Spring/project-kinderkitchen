42f1b818afb8697b4f11ffb76ab7243ec1d280e9
unknown <HunterLaut@gmail.com>
2022-03-31 22:00:22 -0400

    Added delete function. Read needs to be completed before Intigration

--- kinderkitchen/screens/RecipeSaved.js
+++ kinderkitchen/screens/RecipeSaved.js
@@ -1,9 +1,16 @@
+import { getDatabase, ref, remove } from "firebase/database";
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
 import MyNavMenu from "../nav-bar/MyNavMenu";
 
 const RecipeSaved = () => {
+
+function deleteSavedRecipe(recipeName){
+  alert(`Deleting ${recipeName}. This Cannot be Undone! \nSecondary Confirm To be Initialized`);
+  remove(ref(getDatabase(), `users/${currentUserID}/savedRecipes/${recipeName}`));
+}
+
   return (
     <View style={styles.container}>
       <View style={styles.body}>
