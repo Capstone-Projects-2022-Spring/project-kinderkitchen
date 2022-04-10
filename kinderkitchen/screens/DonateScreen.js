7e6b602d33e2280e99ea78ae6e2d7a17fdc80918
unknown <HunterLaut@gmail.com>
2022-04-10 16:59:00 -0400

    Map now Moves with Pin

--- kinderkitchen/screens/DonateScreen.js
+++ kinderkitchen/screens/DonateScreen.js
@@ -116,10 +116,10 @@ const DonateScreen = ({ navigation }) => {
           />
           <MapView style={styles.map}
             initialRegion={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
+              latitude: currentLatitude,
+              longitude: currentLongitude,
+              // latitudeDelta: 0.0922,
+              // longitudeDelta: 0.0421,
             }}
           >
             <Marker 
