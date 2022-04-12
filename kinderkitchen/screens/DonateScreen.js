792db0c24e6bc16c39f06a22cca5b46a547a6ad5
unknown <HunterLaut@gmail.com>
2022-04-11 22:46:55 -0400

    Modal for selecting Address

--- kinderkitchen/screens/DonateScreen.js
+++ kinderkitchen/screens/DonateScreen.js
@@ -9,11 +9,16 @@ import {
   Alert,
   Button,
   Dimensions,
+  Modal,
+  Pressable
 } from "react-native";
 
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import * as Location from 'expo-location';
 import MapView, { Marker, Callout, AnimatedRegion, Animated } from 'react-native-maps';
+
+import { MaterialIcons } from "@expo/vector-icons";
+
 
 import MyNavMenu from "../nav-bar/MyNavMenu";
 
@@ -21,10 +26,12 @@ const DonateScreen = ({ navigation }) => {
 
   const API_KEY = '5f20539b0f6383f1f2118885331abaecfa3e003ec92a5febc2a45707e758667f';
 
+  const [modalVisible, setModalVisible] = useState(false);
   const [locationServiceEnabled, setLocationServiceEnabled] = useState(false);
   const [currentLatitude, setCurrentLatitude] = useState(37.78825);
   const [currentLongitude, setCurrentLongitude] = useState(-122.4324);
   const [newMarkerAddress, setNewMarkerAddress] = useState();
+  const [pressedAddress, setPressedAddress] = useState();
   const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
     'Wait, we are fetching you location...'
   );
@@ -85,6 +92,8 @@ const DonateScreen = ({ navigation }) => {
       }
     }
   };
+
+
   
 
   return (
@@ -92,6 +101,7 @@ const DonateScreen = ({ navigation }) => {
       <View style={styles.body}>
       <TextInput
         placeholder= {displayCurrentAddress}
+        editable = {false}
 
         ></TextInput>
                
@@ -140,7 +150,11 @@ const DonateScreen = ({ navigation }) => {
                 coordinate={{latitude: region.latitude, longitude: region.longitude}}
               >
                 <Callout
-                onPress={() => alert(newMarkerAddress)}>
+                onPress={() => {
+                  setPressedAddress(newMarkerAddress);
+                  setModalVisible(true);
+                  
+                }}>
                   
                   <Text>GetAddress</Text>
                   
@@ -157,6 +171,51 @@ const DonateScreen = ({ navigation }) => {
             </Marker>
           </MapView>
       </View>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+
+            <MaterialIcons
+              name="close"
+              size={24}
+              style={{ ...styles.modalToggle, ...styles.modalClose }}
+              onPress={() => setModalVisible(false)}
+            />
+
+            {/*Header*/}
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalText}>Address</Text>
+            </View>
+
+            {/*Address*/}
+            <View>
+            <Text style={styles.modalText}>{pressedAddress}</Text>
+            </View>
+
+            {/*ButtonField*/}
+            <View style={styles.submissionField}>
+              {/*Submit Button*/}
+              <Pressable
+                style={[styles.button, styles.buttonSubmit]}
+                onPress={() => {
+                  setModalVisible(!modalVisible);
+                  alert("This will navigate to Doanation Page Stay Tuned!");
+                  //navigation.navigate("Category", pressedAddress);//Navigate to Donation Selection
+                }}>
+                <Text style={styles.textStyle}>Submit</Text>
+              </Pressable>
+
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
     <MyNavMenu />
     </View>
@@ -220,6 +279,86 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
+  },
+  modalHeader: {
+    width: "100%",
+    padding: 35,
+    paddingTop: 10,
+    paddingBottom: 0,
+    marginBottom: 10,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+  },
+
+  //Bottom Section to Hold Buttons
+  submissionField: {
+    width: "100%",
+    flexDirection: "row",
+    borderTopColor: "gray",
+    borderTopWidth: 1,
+  },
+
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+
+  buttonSubmit: {
+    width: "33%",
+    marginTop: 10,
+    marginHorizontal: 20,
+    backgroundColor: "#12CDD4",
+  },
+
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+  modalToggle: {
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#f2f2f2",
+    padding: 8,
+    top: 7,
+    right: 120,
+    borderRadius: 10,
+    //alignSelf: 'center',
+  },
+  modalClose: {
+    marginTop: 10,
+    marginBottom: 0,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+
+  //The Pop-Up Box
+  modalView: {
+    width: "80%",
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    paddingBottom: 15,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
   },
 });
 
